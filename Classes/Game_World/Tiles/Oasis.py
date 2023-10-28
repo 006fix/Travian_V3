d69a16e305ab2b0eb617588729240bf2b83598bf
Alan Pye <pye.a.c.a@gmail.com>
2023-10-28 20:41:45 +0100

    adding next core elements

---
+++ Classes/Game_World/Tiles/Oasis.py
@@ -0,0 +1,58 @@
+
+import Classes.Game_World.Tiles.Basic_Tile as base_tile
+import random
+# this class serves as the holder for the slightly more advanced type of square, the oasis
+# the oasis can be owned by players, gathers animals, and gathers resources into itself
+# it can also be of various types.
+
+class Oasis(base_tile.Square):
+    def __init__(self, location):
+        super().__init__(location)
+        # adding in a "resource_types" field, to determine type of oasis
+        self.resource_types = []
+        # 25% odds to be each type of resource
+        # additional 20% odds to hold a secondary crop type
+        randval = random.randint(1, 100)
+        if randval <= 25:
+            self.resource_types.append('wood')
+        elif randval <= 50:
+            self.resource_types.append('clay')
+        elif randval <= 75:
+            self.resource_types.append('iron')
+        else:
+            self.resource_types.append('crop')
+        # second iteration to handle secondary crop types
+        randval = random.randint(1, 100)
+        if randval > 80:
+            self.resource_types.append('crop')
+
+        # now we define the storage holdings of the oasis and storage cap, and refill rate
+        #as always : storage is defined as ['wood','clay','iron','crop']
+        # if only one type of resource, cap = 1k of each. if 2, 2k of each
+        if len(self.resource_types) == 1:
+            self.storage_cap = [1000, 1000, 1000, 1000]
+        else:
+            self.storage_cap = [2000, 2000, 2000, 2000]
+        # we also need to define a refill rate (figures given per hour). This is base 10 of each. If double crop, 10 of each 80 of crop
+        self.default_refill_rate = {'wood': 10, 'clay': 10, 'iron': 10, 'crop': 10}
+        if self.resource_types == ['crop','crop']:
+            self.refill_rate = [10, 10, 10, 80]
+        else:
+            for item in self.resource_types:
+                self.default_refill_rate[item] = 40
+            self.refill_rate = [self.default_refill_rate['wood'], self.default_refill_rate['clay'],
+                           self.default_refill_rate['iron'], self.default_refill_rate['crop']]
+
+        # initial storage is the maximum possible storage.
+        self.resource_storage = self.storage_cap
+
+        # to do later : add function to limit withdrawls via raids from any individual player
+
+        # to do later : add in oasis defenders, actually make these defenders.
+
+        # this serves to be updated as required to trigger the oasis to update its stats
+        self.next_action = False
+
+    # to do later : add in ability for oasis to check its resources and update as needed based on elapsed time since last action
+
+
--- Classes/Game_World/Tiles/Wilderness.py
+++ Classes/Game_World/Tiles/Wilderness.py
@@ -1,5 +1,8 @@
 
 import Classes.Game_World.Tiles.Basic_Tile as base_tile
+# this class simply serves to define the basic "wilderness" class of square
+# this square cannot be interacted with in any way, and is a non entity beyond providing
+# blockages to the map
 
 class Wilderness(base_tile.Square):
     def __init__(self, location):
